681002261d92d776746a49f7fe757e13ddeb49e1
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-10 13:25:12 +0900

    Update salary.c

--- salary.c
+++ salary.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int msal,result;
+int msal,result,hsal,day,time;
 int total=0;
 
 int mon_sal1(int a)
@@ -26,7 +26,15 @@ int mon_sal3(int c)
   return result;
 }
 
-
+int hour_sal(int d)
+{
+  printf("이번달에 일한 일 수를 입력해주세요 :\n");
+  scanf("%d",&day);
+  printf("하루에 일하는 시간을 입력해주세요: \n");
+  scanf("%d",&time);
+  
+         
+  
 
 
 int main(void)
@@ -45,6 +53,7 @@ int main(void)
       case 1:
         printf("세전 월급을 입력해주세요 :\n");
         scanf("%d",&msal);
+        
         mon_sal1(msal);
         printf("국민연금 금액 : %d\n",result);
         mon_sal2(msal);
@@ -54,6 +63,12 @@ int main(void)
         printf("총 세금: %d\n\n",total);
         printf("세 후 월급 : %d원\n",msal-total);
         break;
+        
+      case 2:
+        printf("시급을 입력해 주세요 : ");
+        scanf("%d",&hsal);
+        
+        hour_sal(hsal);
     }
 }
         
