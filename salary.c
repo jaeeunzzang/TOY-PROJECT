a0479d08e566e1192a51c99a1f75186a0bddf8e0
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-07-10 13:39:21 +0900

    Update salary.c

--- salary.c
+++ salary.c
@@ -3,43 +3,42 @@
 
 int msal,result,hsal,day,time;
 int total=0;
+int h_sal=0;
 
-int mon_sal1(int a)
+
+int mon_sal(int a)
 {
-  
   result=a*0.045;
   total+=result;
-  return result;
-}
-
-int mon_sal2(int b)
-{
-  result=b*0.0323;
+  printf("국민연금 금액 : %d\n",result);
+  
+  result=a*0.0323;
   total+=result;
-  return result;
-}
-
-int mon_sal3(int c)
-{
-  result=c*0.0065;
+  printf("건강보험 납부 후 금액 : %d\n",result);
+  
+  result=a*0.0065;
   total+=result;
-  return result;
+  printf("고용보험 납부 후 금액 : %d\n",result);
+  
+  return total;
 }
 
-int hour_sal(int d)
+int hour_sal(int b)
 {
+  
   printf("이번달에 일한 일 수를 입력해주세요 :\n");
   scanf("%d",&day);
   printf("하루에 일하는 시간을 입력해주세요: \n");
   scanf("%d",&time);
+  h_sal=b*time*day;
   
-         
+  return h_sal;
+}
   
 
-
-int main(void)
+void main()
 {
-  int num,hsal;
+  int num;
   
   printf("*** 세 후 월급 계산 프로그램 ***\n\n");
   printf("1.월급으로 계산\n");
@@ -54,21 +53,23 @@ int main(void)
         printf("세전 월급을 입력해주세요 :\n");
         scanf("%d",&msal);
         
-        mon_sal1(msal);
-        printf("국민연금 금액 : %d\n",result);
-        mon_sal2(msal);
-        printf("건강보험 납부 후 금액 : %d\n",result);
-        mon_sal3(msal);
-        printf("고용보험 납부 후 금액 : %d\n",result);
+        mon_sal(msal);     
         printf("총 세금: %d\n\n",total);
         printf("세 후 월급 : %d원\n",msal-total);
         break;
         
       case 2:
-        printf("시급을 입력해 주세요 : ");
+        printf("시급을 입력해 주세요 : \n");
         scanf("%d",&hsal);
         
         hour_sal(hsal);
+        printf("세 전 월급은 %d 원 입니다.\n",h_sal);
+        
+        mon_sal(h_sal);
+        printf("총 세금: %d\n\n",total);
+        printf("세 후 월급 : %d원\n",h_sal-total);
+        break;
+        
     }
 }
         
